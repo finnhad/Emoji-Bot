b56cd417d4c0d7783d6c5981b1376b709a314248
OneStrangeOnion <finn.haddon@gmail.com>
2020-07-12 02:14:55 -0500

    Update TODO

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import discord
+import re
 
 client = discord.Client()
+debug = True
 
 # TODO: run emojiPicker as Cog
+    # https://discordpy.readthedocs.io/en/latest/ext/commands/cogs.html
 # TODO: set up DB
-
+    # figure out scraping
+# TODO: find emojis for missing roles
+# TODO: make sure no missing roles
 
 keyFile = open('key.txt', 'r')
 key = keyFile.readline()
